a8246a23cd6a2cfa3a29cc0f8430c7a105527231
Ludvig Lindholm <ludviglindholm127@gmail.com>
2023-05-27 12:36:22 +0200

    2015 done

--- 2015.hs
+++ 2015.hs
@@ -77,6 +77,91 @@ b2 :: a0 -> b0
 ([]>>=)(\_->[(>=)]) :: (Ord a) => [a -> a -> Bool]
 a1 = p
 b1 = a -> a -> Bool
+
+3)
+
+A spark is a bit of code that needs to be evaluated to whnf, the spark is put in a queue and a many threads will take one spark at a time to work on.
+
+seq suggests that the first element is to be made into a spark and ran and that the right side should be returned. 
+
+pseq says is like seq but promises that the first part is ran before the second part is returned.
+
+par turns left and right side into sparks but the left side is suggested to be ran before the right side. the snd arg value is returned
 -}
 
+{-
 
+ORKADE INTE HELA DEN ÄR HYFSAT ENKEL NÄR MAN KOMMER IGÅNG TAR DOCK LÅNG TID ATT GÖRA MEN BÖR BLI I STORT RÄTT
+
+data CircList = CircList [Int] -- List Current Length
+
+append :: CircList -> Int -> CircList
+append (CircList xs) val = (CircList (xs ++ [val]))
+
+removeFirst :: CircList -> CircList
+removeFirst (CircList []) = (CircList [])
+removeFirst (CircList (_:xs)) = (CircList xs)
+
+  
+perimeter :: CircList -> Int
+perimeter (CircList xs) = length xs
+
+currentelem :: CircList -> Maybe Int
+currentelem (CircList xs) = 
+	if length xs > 0 then 
+		Just((head xs))
+	else
+		Nothing
+
+nextelem :: CircList -> Maybe Int
+nextelem (CircList xs) = 
+	if length > 1 then 
+		Just (xs !! 1)
+	else
+		Nothing 
+
+previouselem :: CircList -> Maybe Int
+previouselem (CircList xs)  = 
+	if length xs > 0 then 
+		Just(xs !! ((length xs)-1))
+	else
+		Nothing
+		
+ --Take one from the front and add to the back
+forward :: CircList -> Maybe CircList
+forward cl = append (removeFirst cl) (fromMaybe (currentelem cl))
+
+doStep :: CircList -> (Int, CircList)
+doStep cl = 
+	(currentelem new, new)
+	where new = forward cl
+	
+
+
+insert :: CircList -> Int -> CircList
+insert (CircList xs) val = (CircList (val:xs))
+takefromCl :: Int -> CircList -> [Int]
+takefromCl n (CircList xs) = map fst (take n (iterate (\(i, cl) -> doStep cl)))
+
+-}
+
+g :: (Monad m) => m a -> m b -> m (a, b)
+g x y = do
+	a <- x
+	b <- y
+	return (a, b)
+
+{-
+
+5) 
+g (Just 5) Nothing = Nothing
+g [1, 2, 3] [5, 6, 7] = [(1, 5), (1, 6), (1, 7), (2, 5), (2, 6), (2, 7), (3, 5), (3, 6), (3, 7)]
+
+g (Just "I am") (Just "Charlie") = Just (("I am", "Charlie"))
+
+
+-}
+
+-- 6) filter :: (a -> Bool) -> [a] -> [a]
+filter2 :: (a -> Bool) -> [a] -> [a]
+filter2 f xs = foldr (\elem acc -> if f elem then elem:acc else acc) [] xs
